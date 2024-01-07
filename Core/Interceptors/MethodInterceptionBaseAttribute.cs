b4cb81f2eb9c8611d96cf4bb3021650d4b41ce7c
Barış Kortun <bariskorun@gmail.com>
2024-01-07 13:35:54 +0300

    Interceptor attribute was added

---
+++ Core/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Castle.DynamicProxy;
+
+namespace Core.Interceptors;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+public class MethodInterceptionBaseAttribute : Attribute,IInterceptor
+{
+    public int Priority { get; set; }
+
+    public virtual void Intercept(IInvocation invocation)
+    {
+    }
+}
